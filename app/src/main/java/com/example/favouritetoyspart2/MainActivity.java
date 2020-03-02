acfe63a47aef9a2e60ad9b238ed24a7a5a184488
marthaserpak <m.v.serpak@gmail.com>
2020-03-02 16:03:19 +0200

    Content Provider

---
+++ app/src/main/java/com/example/favouritetoyspart2/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.favouritetoyspart2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCurrentState;
+
+    private Button mButton;
+
+    // This state is when the word definition is hidden and clicking
+    // the button will therefore show the definition
+    private final int STATE_HIDDEN = 0;
+
+    // This state is when the word definition is shown and clicking the
+    // button will therefore advance the app to the next word
+    private final int STATE_SHOWN = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.button_next);
+    }
+
+    /**
+     * This is called from the layout when the button is clicked and switches between the
+     * two app states.
+     * @param view The view that was clicked
+     */
+    public void onButtonClick(View view) {
+
+        // Either show the definition of the current word, or if the definition is currently
+        // showing, move to the next word.
+        switch (mCurrentState) {
+            case STATE_HIDDEN:
+                showDefinition();
+                break;
+            case STATE_SHOWN:
+                nextWord();
+                break;
+        }
+    }
+
+    public void nextWord() {
+
+        // Change button text
+        mButton.setText(getString(R.string.show_definition));
+
+        mCurrentState = STATE_HIDDEN;
+
+    }
+
+    public void showDefinition() {
+
+        // Change button text
+        mButton.setText(getString(R.string.next_word));
+
+        mCurrentState = STATE_SHOWN;
+
+    }
+
+}
